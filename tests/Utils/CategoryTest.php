bd8c47e53d1e25fd8a1befb291327111c35e5021
giroberto <umbertobarros@gmail.com>
2021-03-31 15:33:55 -0300

    Category Test Fix

--- tests/Utils/CategoryTest.php
+++ tests/Utils/CategoryTest.php
@@ -4,21 +4,32 @@ namespace App\Tests\Utils;
 
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use App\Twig\AppExtension;
+use App\Utils\CategoryTreeFrontPage;
 
 class CategoryTest extends KernelTestCase
 {
     protected $mockedCategoryTreeFrontPage;
+    protected $mockedCategoryTreeAdminList;
+    protected $mockedCategoryTreeAdminOptionList;
 
     protected function setUp(): void
     {
         $kernel = self::bootKernel();
         $urlgenerator = $kernel->getContainer()->get('router');
-        $this->mockedCategoryTreeFrontPage = $this->getMockBuilder('App\Utils\CategoryTreeFrontPage')
-            ->disableOriginalConstructor()
-            ->setMethods() // if no, all methods return null unless mocked
-            ->getMock();
+        $test_classes = [
+            'CategoryTreeFrontPage',
+            'CategoryTreeAdminList',
+            'CategoryTreeAdminOptionList',
+        ];
+        foreach ($test_classes as $class) {
+            $name = "mocked" . $class;
+            $this->$name = $this->getMockBuilder('App\\Utils\\' . $class)
+                ->disableOriginalConstructor()
+                ->onlyMethods([]) // if no, all methods return null unless mocked
+                ->getMock();
 
-        $this->mockedCategoryTreeFrontPage->urlgenerator = $urlgenerator;
+            $this->$name->urlgenerator = $urlgenerator;
+        }
     }
 
     /**
@@ -32,6 +43,28 @@ class CategoryTest extends KernelTestCase
         $array = $this->mockedCategoryTreeFrontPage->buildTree($main_parent_id);
         $this->assertSame($string, $this->mockedCategoryTreeFrontPage->getCategoryList($array));
     }
+
+
+    /**
+     * @dataProvider dataForCategoryTreeAdminOptionsList
+     */
+    public function testCategoryTreeAdminOptionsList($arrayToCompare, $arrayFromDb)
+    {
+        $this->mockedCategoryTreeAdminOptionList->categoriesArrayFromDb = $arrayFromDb;
+        $arrayFromDb = $this->mockedCategoryTreeAdminOptionList->buildTree();
+        $this->assertSame($arrayToCompare, $this->mockedCategoryTreeAdminOptionList->getCategoryList($arrayFromDb));
+    }
+
+    /**
+     * @dataProvider dataForCategoryTreeAdminList
+     */
+    public function testCategoryTreeAdminList($string, $array)
+    {
+        $this->mockedCategoryTreeAdminList->categoriesArrayFromDb = $array;
+        $array = $this->mockedCategoryTreeAdminList->buildTree();
+        $this->assertSame($string, $this->mockedCategoryTreeAdminList->getCategoryList($array));
+    }
+
 
     public function dataForCategoryTreeFrontPage()
     {
@@ -78,6 +111,31 @@ class CategoryTest extends KernelTestCase
             ],
             14
 
+        ];
+    }
+
+    public function dataForCategoryTreeAdminOptionsList()
+    {
+        yield [
+            [
+                ['name' => 'Electronics', 'id' => 1],
+                ['name' => '--Computers', 'id' => 6],
+                ['name' => '----Laptops', 'id' => 8],
+                ['name' => '------HP', 'id' => 14]
+            ],
+            [
+                ['name' => 'Electronics', 'id' => 1, 'parent_id' => null],
+                ['name' => 'Computers', 'id' => 6, 'parent_id' => 1],
+                ['name' => 'Laptops', 'id' => 8, 'parent_id' => 6],
+                ['name' => 'HP', 'id' => 14, 'parent_id' => 8]
+            ]
+        ];
+    }
+
+    public function dataForCategoryTreeAdminList()
+    {
+        yield [
+            '<ul class="fa-ul text-left"><li><i class="fa-li fa fa-arrow-right"></i>  Toys<a href="/admin/edit-category/2"> Edit</a> <a onclick="return confirm(\'Are you sure?\');" href="/admin/delete-category/2">Delete</a></li></ul><ul class="fa-ul text-left"><li><i class="fa-li fa fa-arrow-right"></i>  Toys<a href="/admin/edit-category/2"> Edit</a> <a onclick="return confirm(\'Are you sure?\');" href="/admin/delete-category/2">Delete</a></li></ul>', [['id' => 2, 'parent_id' => null, 'name' => 'Toys']]
         ];
     }
 }
